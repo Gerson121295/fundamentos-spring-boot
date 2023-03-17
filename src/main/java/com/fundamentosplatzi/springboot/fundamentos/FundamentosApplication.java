b9ef942a611baeebf29964350d81cd1dcadf8215
Gerson Ep <gerson21millones@gmail.com>
2023-03-16 23:09:15 -0600

    16 Que son los logs y como usarlos

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,8 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +15,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner { /* Implementa la interfaz CommandLineRunner*/
+
+	//Ejemplo de log: libreria apache-commons
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class); // se ele envia la clase en la que nos encontramos
+
 
 	private ComponentDependency componentDependency; /*inyectar la dependencia con spring boot, y agrega el nombre componentDependency */
 	private MyBean myBean; 	/*inyecto la dependecia*/
@@ -47,5 +53,13 @@ public class FundamentosApplication implements CommandLineRunner { /* Implementa
 		myBeanWithDependency.printWithDependency(); //implementacion es la que esta en la clase
 		System.out.println(myBeanWithProperties.function());//llamado de la funcion(devuelve nombre+apellido) implementada en esa propiedad
 		System.out.println(userPojo.getEmail()+"-"+userPojo.getPassword()+"-"+userPojo.getAge());
+
+		//ejemplo de log
+		try {
+			int value = 10/0;		//Generamos un error al dividir
+			LOGGER.info("Mi valor: " +value); // esto no se ejecutará porqur anterior hubo un error pasa al catch
+		}catch (Exception e){
+			LOGGER.error("Esto es un error por dividir en 0"+e.getMessage());  //llamado del log nivel de error
+		}//en consola vemos los log.Crtl + F : y pegar el mensaje "Esto es un error por dividir en 0" para ver el error
 	}
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+
+    Log  LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
     private MyOperation myOperation; //llamado de la dependencia
 
@@ -12,8 +17,9 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
-
+        LOGGER.info("Hemos ingresado al metodo printWhitDependency");
         int numero=1;
+        LOGGER.debug("El numero enviado como parametro a la dependencia operation es :"+numero);
         System.out.println(myOperation.sum(numero)); //imprime llamado de la operacion, de la dependencia
         System.out.println("Hi desde la implementacion de un bean con dependencia");
     }
