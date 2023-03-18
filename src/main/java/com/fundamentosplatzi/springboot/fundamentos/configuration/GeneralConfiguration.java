12f650f7ae6db5c23270544be6717d947891c87a
Gerson Ep <gerson21millones@gmail.com>
2023-03-18 10:27:30 -0600

    18 Configuracion de dataSource con properties y clases. Config. para la connexion a la BD

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImp
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //uso de properties y valores
 @Configuration
@@ -26,4 +29,23 @@ public class GeneralConfiguration {
     public MyBeanWithProperties function(){ //MyBeanWithProperties es la interfaz
         return new MyBeanWithPropertiesImplement(name, apellido); // MyBeanWithPropertiesImplement es la implementacion es la clase que representa a ese bean
     }
+
+    // Configuracion Manual para la conexion a la base de datos
+    //Cuando inyectemos la funcion DataSource ya podremos usar toda esa configuracion
+    @Bean
+    //Dentro de este metodo relaciona toda la configuracion a la BD
+    public DataSource dataSource(){ //Llamamos DataSource y le nombramos dataSource
+        DataSourceBuilder dataSourceBuilder=DataSourceBuilder.create(); //llama al metodo de DataSourceBuilder.create();
+        //La sig. configuracion es la misma que se agregaria en application.properties si se hiciese la config en ese archivo
+        dataSourceBuilder.driverClassName("org.H2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build(); //Retornamos la implementacion
+    }
+
+
+
+
+
 }
